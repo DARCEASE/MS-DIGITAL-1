802cabbf78442574193b4b8b81786d655fe7f6a0
Darcy <Darcelisgutierrez@gmail.com>
2021-09-22 13:20:56 -0400

    Doodle Jump Base Completed!!!!
    Can Jump w/ Bubbles and can reach the top

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,11 +4,24 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject platformPrefab;
+    public int platformCount = 200; //How Many total "bubbles are going to spawn"
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (int i = 0; i < platformCount; i++)
+        {
+            Vector3 spawnPosition = new Vector3();
+            spawnPosition.y += Random.Range(-10f, 50f); // they shoud be vertically this far away from eachother 
+            spawnPosition.x += Random.Range(-10f, 10f); // they should horizontally spawn this far away from eachother 
+            Instantiate(platformPrefab, spawnPosition, Quaternion.identity);
+        }
+
+
     }
+
 
     // Update is called once per frame
     void Update()
